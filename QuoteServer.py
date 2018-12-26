e2277e122b38a46a3d447fc9c593c967ec10ff73
shobh <shobhitgoel01@gmail.com>
2018-12-26 17:53:26 +0530

    merging json files

--- QuoteServer.py
+++ QuoteServer.py
@@ -8,11 +8,8 @@ app = Flask(__name__)
 
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-json_url = os.path.join(SITE_ROOT, 'buddha-quotes.json')
-data = json.load(open(json_url, encoding="latin-1"))
-
-json_url2 = os.path.join(SITE_ROOT, 'marcus-aurelius-quotes.json')
-data2 = json.load(open(json_url2, encoding="latin-1"))
+json_url = os.path.join(SITE_ROOT, 'data.json')
+data = json.load(open(json_url, encoding="utf-8"))
 
 
 
@@ -22,16 +19,11 @@ def index():
 
 @app.route('/api/types', methods=['GET'])
 def get_types():
-    return "2"
+    return "1"
 	
 @app.route('/api/quote', methods=['GET'])
 def get_quote():
     return jsonify(data)
-	
-@app.route('/api/quote2', methods=['GET'])
-def get_quote2():
-    return jsonify(data2)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
